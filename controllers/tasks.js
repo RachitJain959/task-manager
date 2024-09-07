cd0066343ff5eaefd1c69d858394522f4b81986d
Rachit Jain <rachitjain9596@gmail.com>
2024-09-07 13:26:12 +0530

    asyncWrapper-refactor delete&updateTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,7 +12,9 @@ const createTask = asyncWrapper(async (req, res) => {
 });
 
 const getTask = asyncWrapper(async (req, res) => {
-  const tasks = await Task.findOne({ _id: taskID });
+  const { id: taskID } = req.params;
+
+  const task = await Task.findOne({ _id: taskID });
 
   if (!task) {
     return res.status(404).json({ msg: `no task with id: ${taskID} found!` });
@@ -20,35 +22,29 @@ const getTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
 
-    if (!task) {
-      return res.status(404).json({ msg: `no task with id: ${taskID} found!` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  const task = await Task.findOneAndDelete({ _id: taskID });
+
+  if (!task) {
+    return res.status(404).json({ msg: `no task with id: ${taskID} found!` });
   }
-};
+  res.status(200).json({ task });
+});
 
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
 
-    if (!task) {
-      return res.status(404).json({ msg: `no task with id: ${taskID} found!` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return res.status(404).json({ msg: `no task with id: ${taskID} found!` });
   }
-};
+  res.status(200).json({ task });
+});
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
